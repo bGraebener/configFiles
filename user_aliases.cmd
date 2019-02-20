14928c62399bfd8893cd783037bcf76f03da263a
bGraebener <bastiangraebener@gmail.com>
2019-02-20 13:32:34 +0000

    add cmder user_aliases

---
+++ user_aliases.cmd
@@ -0,0 +1,23 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+e =explorer $1
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+ll=ls -al --color $*
+pwd=cd
+clear=cls
+history=cat "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+g.=git add .
+gp=git push
+gs=git status
+gll=git pull
+gc=git commit -m "$*"
+conf=cd /d "c:\Users\Basti\Coding\configFiles"
